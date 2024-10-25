586176efad51c53a933cbd03aacc5ce47c4c684f
timmysql <timkonecky@outlook.com>
2024-10-25 08:37:25 -0600

    first commit

---
+++ sitesearch/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from django.contrib import admin
+# from sitesearch.views import SiteSearchView
+# from django.views.generic import TemplateView
+
+from . import views
+
+
+app_name = "sitesearch"
+urlpatterns = [
+
+    # ex: /polls/
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"),  
+    # path("sitesearch/", SiteSearchView.as_view()),
+    # path("about/", SiteSearchView.as_view(template_name="about.html")),    
+    
+    # path("", views.sexysearch_index, name="sexysearch_index"),  
+    # path("sexysearch", views.sexysearch, name="sexysearch"),        
+
+    # # ex: /polls/5/
+    # path("<int:organization_id>/", views.detail, name="detail"),
+    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # path("<int:organization_id>/entry/", views.entry, name="entry"),
+
+]
